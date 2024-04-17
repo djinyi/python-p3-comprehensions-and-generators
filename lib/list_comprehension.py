c9e23994a7aeb214080ae42c8d5007ee03f55a21
Daisy Yi <daisyyi@rocketmail.com>
2024-04-17 12:47:44 -0700

    pass test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n%2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    return [str+"!" for str in sentence_list]
